191f5a2160e458c92f6b19cc12a3575d4d156b3c
Simon Zoltán <zoltan.simon@v9.hu>
2026-03-15 00:13:23 +0100

    hianyzo fileok

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <title><?= $this->renderSection('title') ?></title>
     
-    <link rel="stylesheet" href="<?= base_url('css/vendor/bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('css/lib/bootstrap.min.css') ?>">
     <?= $this->renderSection('styles') ?>
 </head>
 <body>
 
     <?= $this->renderSection('content') ?>
 
-    <script src="<?= base_url('js/vendor/vue.global.js') ?>"></script>
-    <script src="<?= base_url('js/vendor/bootstrap.bundle.min.js') ?>"></script>
+    <script src="<?= base_url('js/lib/vue.global.js') ?>"></script>
+    <script src="<?= base_url('js/lib/bootstrap.bundle.min.js') ?>"></script>
 
     <?= $this->renderSection('scripts') ?>
 </body>
